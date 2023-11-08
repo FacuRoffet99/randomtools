9f73a4e8af0a2a9a88314c97d728350848f85e20
FacuRoffet99 <facundo.roffet@cs.uns.edu.ar>
2023-11-08 18:42:00 +0000

    added ram_usage in memory.py

---
+++ tools/memory.py
@@ -0,0 +1,20 @@
+import sys
+
+def sizeof_fmt(num, suffix='B'):
+    ''' by Fred Cirera,  https://stackoverflow.com/a/1094933/1870254, modified'''
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f %s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f %s%s" % (num, 'Yi', suffix)
+
+def ram_usage(max_vars=None):
+    '''
+    Prints a list of variables and the space each one takes in memory (RAM).
+    
+    max_vars: int, maximum number of variables to display (default: None)
+    '''
+    j = 5
+    for name, size in sorted(((name, sys.getsizeof(value)) for name, value in list(
+                              globals().items())), key= lambda x: -x[1])[:max_vars]:
+        print("{:>30}: {:>8}".format(name, sizeof_fmt(size)))
